523ae06a00904ed9d21658c29cfc07aa8538284e
silentShulk <marcostrada258@gmail.com>
2026-07-19 19:30:49 +0200

    Added animation for start

---
+++ src/00-Inizio/just_like_a_star.py
@@ -0,0 +1,19 @@
+from manim.animation.creation import Write
+from manim.animation.fading import FadeIn
+from manim.constants import DOWN, UP
+from manim.mobject.text.tex_mobject import Tex
+from manim.scene.scene import Scene
+from manim.utils.color.manim_colors import RED
+
+class JustLikeAStar(Scene):
+    def construct(self) -> None:
+        frase = Tex("Just like a star across my sky", font_size=90, color=RED)\
+            .to_edge(UP, buff=2)
+        frase_ita = Tex(r"Proprio come una stella\\attraverso il mio celo", font_size=70)\
+            .to_edge(DOWN, buff=2)
+
+        self.play(Write(frase))
+        self.play(FadeIn(frase_ita))
+
+
+        self.wait(2)
